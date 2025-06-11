37237ff62d73a2f423d651a4641de53a57bb0970
simplyKai-pyt <wonderouskai@gmail.com>
2025-06-11 14:47:18 -0400

    Create temperature_converter.py

---
+++ temperature_converter.py
@@ -0,0 +1,7 @@
+def celsius(temp)
+  x = (temp - 32) * (5/9)
+  print(f"Your temp is {x} degrees Celsius.")
+
+def fahrenheit(temp)
+  x = (temp * (9/5)) + 32)
+  print(f"Your temp is {x} degrees Fahrenheit.")
